633ebc7210fc1d66a940fe612ddb8aacd7da050c
m0h1m3d <mohisham33@gmail.com>
2024-03-27 04:09:02 +0000

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output:{
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
