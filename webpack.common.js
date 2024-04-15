8991fca6c1393595b934a42b051530387c29a63f
m0h1m3d <mohisham33@gmail.com>
2024-04-15 14:27:52 +0100

    fix typo

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
